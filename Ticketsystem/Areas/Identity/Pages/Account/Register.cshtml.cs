16e32b9b4bf412c2f36327d200d549fcd356d223
Jann Emken <j.emken@gmx.net>
2023-02-27 20:34:12 +0100

    neue Benutzer haben automatisch die Rolle "Mitarbeiter"
    - wird direkt in master gepusht, da nur sehr kleine Änderung

--- Ticketsystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ticketsystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -148,6 +148,8 @@ namespace Ticketsystem.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    
+                    await _userManager.AddToRoleAsync(user, Roles.Mitarbeiter.ToString());
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
