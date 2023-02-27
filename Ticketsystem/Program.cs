5cb1368c63888070ccec2da035237b1174dc16db
Jann Emken <j.emken@gmx.net>
2023-02-27 14:25:37 +0100

    Grundgerüst für ASP.NET Core Identity hinzugefügt
    Außerdem:
    - 3 Rollen hinzugefügt (Administrator, Abteilungsleiter, Mitarbeiter)
    - FirstName und LastName zum user model hinzugefügt
    - Login per Username oder Email möglich
    - beim Start der Anwendung wird ein Standard-Admin-User erstellt, sofern dieser nicht existiert:
    
    username:  admin
    password:  Service1234!
    email:     admin@localhost

--- Ticketsystem/Program.cs
+++ Ticketsystem/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Ticketsystem.Areas.Identity.Data;
 namespace Ticketsystem
 {
     public class Program
@@ -6,10 +9,25 @@ namespace Ticketsystem
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var identityConnectionString = builder.Configuration.GetConnectionString("IdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityContextConnection' not found.");
+
+            builder.Services.AddDbContext<IdentityContext>(options => options.UseSqlite(identityConnectionString));
+
+            builder.Services.AddDefaultIdentity<TicketsystemUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<IdentityContext>()
+                .AddDefaultTokenProviders();
+
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            using var scope = app.Services.CreateScope();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<TicketsystemUser>>();
+            ContextSeed.SeedUserRolesAsync(roleManager).Wait();
+            ContextSeed.SeedDefaultAdmin(userManager).Wait();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -24,12 +42,15 @@ namespace Ticketsystem
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
+            app.MapRazorPages();
+            
             app.Run();
         }
     }
