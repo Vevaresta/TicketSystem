74228e43211bd61a818d3b177959aeb454eb263e
QGJ <qgj.devel@gmail.com>
2023-05-09 10:51:36 +0200

    PDF-Upload: Dateityp-Check hinzugefügt

--- Ticketsystem/wwwroot/js/ticketdetails.js
+++ Ticketsystem/wwwroot/js/ticketdetails.js
@@ -46,22 +46,30 @@ $("#button-show-pdf-signed").on("click", function () {
 $('#file-picker-pdf-signed').on('change', function () {
     let ticketId = $("#hidden-ticket-id").val();
     var file = this.files[0];
-    var formData = new FormData();
-    formData.append('pdfFile', file);
 
-    $.ajax({
-        url: '/Tickets/UploadPdf?id=' + ticketId,
-        type: 'POST',
-        data: formData,
-        processData: false,
-        contentType: false,
-        success: function (response) {
-            $("#button-show-pdf-signed").removeClass("collapse");
-        },
-        error: function (error) {
-            alert(response);
-        }
-    });
+
+    if (file.type != 'application/pdf') {
+        alert('Keine gültige PDF-Datei!');
+    }
+    else {
+
+        var formData = new FormData();
+        formData.append('pdfFile', file);
+
+        $.ajax({
+            url: '/Tickets/UploadPdf?id=' + ticketId,
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                $("#button-show-pdf-signed").removeClass("collapse");
+            },
+            error: function (error) {
+                alert(response);
+            }
+        });
+    }
 });
 
 $("#button-send-email").on("click", function (event) {
